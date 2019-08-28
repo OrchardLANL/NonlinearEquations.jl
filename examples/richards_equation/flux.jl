81931b2596c40f2427973fa1a8bf22a3c264f1b1
Daniel O'Malley <omalled@lanl.gov>
2019-08-28 13:59:57 -0600

    more extensive testing of flux derivatives

--- examples/richards_equation/flux.jl
+++ examples/richards_equation/flux.jl
@@ -1,11 +1,12 @@
 #this example is modified from https://numericalenvironmental.wordpress.com/2018/05/26/a-steady-state-variably-saturated-flow-model-in-vertical-cross-section-a-finite-difference-approach-using-julia/
+using Test
 using Flux
 using Flux.Tracker
 using Flux.Tracker: data, TrackedArray, track, @grad
 
-@time include("utilities.jl")
-@time include("inputdeck.jl")
-@time include("ex.jl")
+include("utilities.jl")
+include("inputdeck.jl")
+include("ex.jl")
 
 solveforpsi(Ks::TrackedArray) = track(solveforpsi, Ks)
 
@@ -23,4 +24,34 @@ predict() = solveforpsi(pKs)
 loss() = predict()[obsnode]
 theta = Params([pKs])
 grads = Tracker.gradient(() -> loss(), theta)
-plotgradient(data(grads[pKs]), obsnode)
+@test data(grads[pKs]) ≈ grad
+
+pKs = param(Ks)
+predict() = solveforpsi(pKs)
+loss() = predict()[obsnode] ^ 2
+theta = Params([pKs])
+grads = Tracker.gradient(() -> loss(), theta)
+@test data(grads[pKs]) ≈ 2 * psi[obsnode] * grad
+
+pKs = param(Ks)
+predict() = solveforpsi(exp.(log.(pKs)))
+loss() = predict()[obsnode]
+theta = Params([pKs])
+grads = Tracker.gradient(() -> loss(), theta)
+@test data(grads[pKs]) ≈ grad
+
+logKs = log.(Ks)
+plogKs = param(logKs)
+predict() = solveforpsi(exp.(plogKs))
+loss() = predict()[obsnode]
+theta = Params([plogKs])
+grads = Tracker.gradient(() -> loss(), theta)
+@test data(grads[plogKs]) ≈ grad .* Ks
+
+logKs = log.(Ks)
+plogKs = param(logKs)
+predict() = solveforpsi(exp.(plogKs))
+loss() = predict()[obsnode] ^ 2
+theta = Params([plogKs])
+grads = Tracker.gradient(() -> loss(), theta)
+@test data(grads[plogKs]) ≈ 2 * psi[obsnode] * grad .* Ks
