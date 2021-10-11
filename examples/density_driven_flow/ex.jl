0a08e6a0fccc32d3cc9cc5475bf5d99bc859c642
Daniel O'Malley <omalled@lanl.gov>
2021-10-11 16:47:49 -0600

    i think pressure is fixed, and similar changes needed in transport?

--- examples/density_driven_flow/ex.jl
+++ examples/density_driven_flow/ex.jl
@@ -21,8 +21,10 @@ g = 9.8067#m/s
 #nz = 100
 #nx = 50
 #nz = 5
-nx = 200
-nz = 20
+#nx = 100
+#nz = 10
+nx = 100
+nz = 10
 mins = [0, 0]
 maxs = [1000, 100]
 deltax = (maxs[1] - mins[1]) / nx
@@ -30,7 +32,7 @@ deltaz = (maxs[2] - mins[2]) / nz
 tfinal = 3.1536e10#seconds
 c0 = [j == nz && i * deltax >= 100 && i * deltax <= 500 ? 750.6 : 4.4457e-2 for j = 1:nz, i = 1:nx]#this is similar to the initial conditions in the document, but matches the dirichlet boundary condition
 
-function plot(args...)
+function plot(args...; filename=false)
 	numplots = div(length(args), 2)
 	fig, axs = PyPlot.subplots(1, numplots, figsize=(4 * numplots + 1, 4))
 	for i = 1:numplots
@@ -48,31 +50,39 @@ function plot(args...)
 	fig.tight_layout()
 	display(fig)
 	println()
+	if  filename != false
+		fig.savefig(filename)
+	end
 	PyPlot.close(fig)
 end
 
+#maybe the key is just that it is diffusing in for their example. change the transport boundary conditions so that there’s no CO2 in the reservoir initially, and it only diffuses in
+#still, something weird is going on with the pressure -- gravity seems to be absent from the pressure
+
+z(j) = j * deltaz
 ij2k(i, j) = j + (i - 1) * nz
 @NonlinearEquations.equations function pressure(p, c)
 	NonlinearEquations.setnumequations(length(p))
 	#do the internal nodes
 	for i = 2:nx - 1, j = 1:nz
 		k = ij2k(i, j)
-		if j == nz && i * deltax >= 100 && i * deltax <= 500
-			NonlinearEquations.addterm(k, p[k] - 2e7)
-		else
-			kl = i > 1 ? ij2k(i - 1, j) : k#node to the left
-			kr = i < nx ? ij2k(i + 1, j) : k#node to the right
-			kd = j > 1 ? ij2k(i, j - 1) : k#node below
-			ku = j < nz ? ij2k(i, j + 1) : k#node above
-			NonlinearEquations.addterm(k, (p[kr] - 2 * p[k] + p[kl]) / deltax ^ 2)
-			NonlinearEquations.addterm(k, (p[ku] - 2 * p[k] + p[kd]) / deltaz ^ 2)
-			NonlinearEquations.addterm(k, Γ * (c[ku] - c[kd]) / (2 * deltaz))
-		end
+		ju = j < nz ? j + 1 : j
+		jd = j > 1 ? j - 1 : j
+		il = i > 1 ? i - 1 : i
+		ir = i < nx ? i + 1 : i
+		kl = ij2k(il, j)
+		kr = ij2k(ir, j)
+		kd = ij2k(i, jd)
+		ku = ij2k(i, ju)
+		NonlinearEquations.addterm(k, (p[kr] - 2 * p[k] + p[kl]) / deltax ^ 2)
+		NonlinearEquations.addterm(k, (p[ku] - 2 * p[k] + p[kd]) / deltaz ^ 2)
+		NonlinearEquations.addterm(k, ((ρ_w * g - Γ * c[ku])* (ju - j) - (ρ_w * g - Γ * c[k]) * (j - jd)) * deltaz / deltaz ^ 2)#gravity term
 	end
 	#do the bottom nodes on the east/west boundary
 	k_east = ij2k(nx, nz)
 	k_west = ij2k(1, nz)
-	NonlinearEquations.addterm(k_east, p[k_east] - 1.99998975026e7)
+	#NonlinearEquations.addterm(k_east, p[k_east] - 1.99998975026e7)
+	NonlinearEquations.addterm(k_east, p[k_east] - 2e7)
 	NonlinearEquations.addterm(k_west, p[k_west] - 2e7)
 	#do the east/west boundaries
 	for j = 1:nz - 1
@@ -80,8 +90,12 @@ ij2k(i, j) = j + (i - 1) * nz
 		ku_east = ij2k(nx, j + 1)
 		k_west = ij2k(1, j)
 		ku_west = ij2k(1, j + 1)
-		NonlinearEquations.addterm(k_east, p[k_east] - p[ku_east] - g * (ρ_w * (1 - c[k_east] * V_ϕ) + M * c[k_east]) * deltaz)
-		NonlinearEquations.addterm(k_west, p[k_west] - p[ku_west] - g * (ρ_w * (1 - c[k_west] * V_ϕ) + M * c[k_west]) * deltaz)
+		#NonlinearEquations.addterm(k_east, p[k_east] - p[ku_east] - g * (ρ_w * (1 - c[k_east] * V_ϕ) + M * c[k_east]) * deltaz)
+		#NonlinearEquations.addterm(k_west, p[k_west] - p[ku_west] - g * (ρ_w * (1 - c[k_west] * V_ϕ) + M * c[k_west]) * deltaz)
+		#NonlinearEquations.addterm(k_west, p[k_west] - g * ρ_w * (nz - 1 + j) * deltaz)
+		#NonlinearEquations.addterm(k_east, p[k_east] + g * ρ_w * (nz - 1 + j) * deltaz)
+		NonlinearEquations.addterm(k_west, p[k_west] - 2e7 - g * ρ_w * (nz - 1 - j) * deltaz)
+		NonlinearEquations.addterm(k_east, p[k_east] - 2e7 - g * ρ_w * (nz - 1 - j) * deltaz)
 	end
 end
 @NonlinearEquations.equations function concentration(p, c)
@@ -116,12 +130,26 @@ end
 
 #do explicit time stepping
 dt = 1e4 / nz^2 * 10^2
-c = c0
+c = c0# .+ 1e-3 * rand(size(c0))
 p = zeros(size(c0))
 t = 0.0
 i = 0
+frame_counter = 1
+#=
+	c = zeros(nz, nx)
+	A = pressure_p(zeros(nz, nx), c)
+	b = -pressure_residuals(zeros(nz, nx), c)
+	p = reshape(A \ b, nz, nx)
+	@show hash(p)
+	plot(log10.(c), "log10(concentration)", p, "pressure")
+	error("blah")
+	=#
+A = pressure_p(zeros(nz, nx), c0)
+b = -pressure_residuals(zeros(nz, nx), c0)
+p0 = reshape(A \ b, nz, nx)
+p = p0
 while t < 365 * 10
-	global c, t, p, i
+	global c, t, p, i, frame_counter
 	A = pressure_p(zeros(nz, nx), c)
 	b = -pressure_residuals(zeros(nz, nx), c)
 	lastp = p
@@ -130,11 +158,14 @@ while t < 365 * 10
 	lastc = c
 	c += dt * dc
 	t += dt / 3600 / 24
-	if mod(i, 1000) == 0
+	if mod(i, 100) == 0
 		tstring = round(t; digits=1)
-		plot(log10.(c), "log10(concentration) at t=$tstring days", p, "pressure at t=$tstring days", c - c0, "c-c0")
+		plot(c, "log10(concentration) at t=$tstring", p, "pressure", p - p0, "p-p0", c - c0, "c-c0", p - lastp, "dp", c - lastc, "dc")
+		frame_counter += 1
 	end
 	i += 1
 end
 tstring = round(t; digits=1)
 plot(log10.(c), "log10(concentration) at t=$tstring days", p, "pressure at t=$tstring days", c - c0, "c-c0")
+#plot(log10.(c), "log10(concentration) at t=$tstring days", p, "pressure at t=$tstring days"; filename="figs/frame_$(lpad(frame_counter, 6, '0')).png")
+#run(`ffmpeg -y -r 25 -f image2 -s 1920x1080 -i figs/frame_%06d.png -vcodec libx264 -crf 25  -pix_fmt yuv420p movie.mp4`)
